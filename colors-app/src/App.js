79b5b575018d6c25f3f6a5ba9b535dbccef8da0c
ZacharyMenefee <zcmenefee02@gmail.com>
2022-03-20 19:36:04 -0400

    finished minipalette delete

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -10,23 +10,35 @@ import { generatePalette } from "./colorHelpers";
 class App extends Component {
   constructor(props) {
     super(props);
-    const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
-    this.state = { palettes: savedPalettes || seedColors, };
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
+    this.state = { palettes: savedPalettes || seedColors };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
+    this.deletePalette = this.deletePalette.bind(this);
   }
   findPalette(id) {
     return this.state.palettes.find(function(palette) {
       return palette.id === id;
     });
   }
-  savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] }, 
+  deletePalette(id) {
+    this.setState(
+      st => ({ palettes: st.palettes.filter(palette => palette.id !== id) }),
       this.syncLocalStorage
-      );
+    );
   }
-  syncLocalStorage(){
-    window.localStorage.setItem('palettes', JSON.stringify(this.state.palettes));
+  savePalette(newPalette) {
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  }
+  syncLocalStorage() {
+    //save palettes to local storage
+    window.localStorage.setItem(
+      "palettes",
+      JSON.stringify(this.state.palettes)
+    );
   }
   render() {
     return (
@@ -58,7 +70,11 @@ class App extends Component {
           exact
           path='/'
           render={routeProps => (
-            <PaletteList palettes={this.state.palettes} {...routeProps} />
+            <PaletteList
+              palettes={this.state.palettes}
+              deletePalette={this.deletePalette}
+              {...routeProps}
+            />
           )}
         />
         <Route
