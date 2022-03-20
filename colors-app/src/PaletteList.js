63e3535551928c31ba56cda91b65e998ddfa8704
ZacharyMenefee <zcmenefee02@gmail.com>
2022-03-20 19:47:50 -0400

    responsive design

--- colors-app/src/PaletteList.js
+++ colors-app/src/PaletteList.js
@@ -4,32 +4,32 @@ import { Link } from 'react-router-dom';
 import { withStyles } from "@material-ui/styles";
 import styles from "./styles/PaletteListStyles";
 class PaletteList extends Component {
-  goToPalette(id) {
-    this.props.history.push(`/palette/${id}`);
-  }
-  render() {
-    const { palettes, classes, deletePalette } = this.props;
-    return (
-      <div className={classes.root}>
-        <div className={classes.container}>
-          <nav className={classes.nav}>
-            <h1>React Colors</h1>
-            <Link to='/palette/new'>Create Palette</Link>
-          </nav>
-          <div className={classes.palettes}>
-            {palettes.map(palette => (
-              <MiniPalette
-                {...palette}
-                handleClick={() => this.goToPalette(palette.id)}
-                handleDelete={deletePalette}
-                key={palette.id}
-                id={palette.id}
-              />
-            ))}
+    goToPalette(id) {
+      this.props.history.push(`/palette/${id}`);
+    }
+    render() {
+      const { palettes, classes, deletePalette } = this.props;
+      return (
+        <div className={classes.root}>
+          <div className={classes.container}>
+            <nav className={classes.nav}>
+              <h1 className={classes.heading}>React Colors</h1>
+              <Link to='/palette/new'>Create Palette</Link>
+            </nav>
+            <div className={classes.palettes}>
+              {palettes.map(palette => (
+                <MiniPalette
+                  {...palette}
+                  handleClick={() => this.goToPalette(palette.id)}
+                  handleDelete={deletePalette}
+                  key={palette.id}
+                  id={palette.id}
+                />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
-export default withStyles(styles)(PaletteList);
+  export default withStyles(styles)(PaletteList);
--- colors-app/src/styles/PaletteListStyles.js
+++ colors-app/src/styles/PaletteListStyles.js
@@ -1,37 +1,54 @@
-import { withThemeCreator } from "@material-ui/styles";
-import { CenterFocusStrong } from "@mui/icons-material";
-
+import sizes from "./sizes";
+import bg from "./bg.svg";
 export default {
-    root: {
-      backgroundColor: "blue",
-      height: "100vh",
-      display: "flex",
-      alignItems: "flex-start",
-      justifyContent: "center"
+  root: {
+    height: "100vh",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    /* background by SVGBackgrounds.com */
+    backgroundColor: "#394bad",
+    backgroundImage: `url(${bg})`,
+    overflow: "scroll"
+  },
+  heading: {
+    fontSize: "2rem"
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "80%"
     },
-    container: {
-      width: "50%",
-      display: "flex",
-      alignItems: "flex-start",
-      flexDirection: "column",
-      flexWrap: "wrap"
-    },
-    nav: {
-      display: "flex",
-      width: "100%",
-      justifyContent: "space-between",
-      alignItems: "center",
-      color: 'white',
-      '& a': {
-          color: 'white',
-      }
-    },
-    palettes: {
-      boxSizing: "border-box",
-      width: "100%",
-      display: "grid",
-      gridTemplateColumns: "repeat(3, 30%)",
-      gridGap: "5%"
+    [sizes.down("xs")]: {
+      width: "75%"
     }
-  };
-  
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    color: "white",
+    "& a": {
+      color: "white"
+    }
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "2.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)"
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+      gridGap: "1.4rem"
+    }
+  }
+};
